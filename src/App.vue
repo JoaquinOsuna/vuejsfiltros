76d89acf2a26185ed2ab6744c69070e604a49085
Joaquin <joaquin.zatarain@cceo.com.mx>
2021-12-16 11:20:51 -0600

    Se agregaron filtros globales y locales

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,44 @@
 <template>
-  <div id="app">
-    
+  <div id="app" class="container">
+    <div class="bg-secondary text-white rounded">
+      <span>
+        {{ objeto.mensaje | mayusculas }}
+      </span>
+      <hr>
+      <span>
+        {{ objeto.mensaje | suspensivos | mayusculas}}
+      </span>
+      <hr>
+      <span>
+        {{ objeto.temperatura | aFa}}
+      </span>
+      <hr>
+      <span>
+        {{objeto.numero | sumarcinco}}
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
- 
+  data() {
+    return {
+      objeto: {
+        mensaje: 'Hola este es el mensaje, aprende vuejs',
+        temperatura : 20,
+        numero: 5
+      }      
+    }
+  },
+  filters: {
+    mayusculas(mensaje){
+      return  mensaje.toUpperCase();
+    },
+    aFa(temperatura){
+      return (9/5 * temperatura) + 32 + ' F'
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.filter('suspensivos', function(texto){
+  return texto.substring(0, 10) + '...'
+})
+
+Vue.filter('sumarcinco', function(numero){
+  return 'total=' + (numero+5)
+})
 new Vue({
   el: '#app',
   render: h => h(App)
